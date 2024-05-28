384da6515863aa8c05b231979d71e59e4fd96f6a
Isadora Ramos <isadorachb@gmail.com>
2024-05-28 10:56:44 -0300

    Create Patient model

---
+++ lib/models/patient.dart
@@ -0,0 +1,8 @@
+class Patient{
+  final String name;
+  final int age;
+  final String sex;
+  final int medicalRecord; 
+  
+  Patient({required this.name, required this.age, required this.sex, required this.medicalRecord});
+}
