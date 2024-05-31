16a7da972c0c981499e8290bc8e50d4fe636ac73
Isadora Ramos <isadorachb@gmail.com>
2024-05-31 09:06:00 -0300

    Setup routes

---
+++ lib/routes.dart
@@ -0,0 +1,16 @@
+import 'package:clini_dor/pages/home_page.dart';
+import 'package:clini_dor/pages/patients_page.dart';
+import 'package:go_router/go_router.dart';
+
+final routes = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage()
+    ),
+    GoRoute(
+      path: '/patients',
+      builder: (context, state) => const PatientsPage()
+    )
+  ]
+);
