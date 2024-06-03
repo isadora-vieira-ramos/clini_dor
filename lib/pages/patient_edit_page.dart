40bc9223098c68ee8834483467fa65a5abad19df
Isadora Ramos <isadorachb@gmail.com>
2024-06-03 10:01:14 -0300

    Create patient edit page

---
+++ lib/pages/patient_edit_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PatientEditPage extends StatelessWidget {
+  const PatientEditPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 0,
+        foregroundColor: Colors.white,
+        title: Text(
+          "Editar Paciente",
+          style: GoogleFonts.josefinSans(
+            fontSize: 20,
+            color: Colors.white
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text('Edição'),
+      ),
+    );
+  }
+}
