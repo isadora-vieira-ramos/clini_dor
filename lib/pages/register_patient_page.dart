513524cca5fbf200b61d21149832f53e2f074959
Isadora Ramos <isadorachb@gmail.com>
2024-06-03 11:40:12 -0300

    Create new patient

--- lib/pages/register_patient_page.dart
+++ lib/pages/register_patient_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
@@ -12,6 +14,8 @@ class RegisterPatientPage extends StatefulWidget {
 class _RegisterPatientPageState extends State<RegisterPatientPage> {
 
   final TextEditingController _controller = TextEditingController();
+  String dropdownValueSex = 'Masculino';
+  String dropdownValueStudy = 'Fundamental incompleto';
 
   onTapFunction({required BuildContext context}) async {
     DateTime? pickedDate = await showDatePicker(
@@ -42,35 +46,141 @@ class _RegisterPatientPageState extends State<RegisterPatientPage> {
       ),
       body: Container(
         padding: const EdgeInsets.all(25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: ListView(
+          padding: const EdgeInsets.all(10),
           children: [
-            const Text(
-              'Novo Paciente',
-              style: TextStyle(
-                fontSize: 20
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Nome',
+                ),
               ),
             ),
-            const TextField(
-              decoration: InputDecoration(
-                border:OutlineInputBorder(),
-                labelText: 'Nome'
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Prontuário'
+                ),
               ),
             ),
-            const TextField(
-              decoration: InputDecoration(
-                border:OutlineInputBorder(),
-                labelText: 'Prontuário'
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                controller: _controller,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Data de nascimento'
+                ),
+                onTap: () => onTapFunction(context: context),
               ),
             ),
-            TextField(
-              controller: _controller,
-              readOnly: true,
-              decoration: const InputDecoration(
-                border:OutlineInputBorder(),
-                labelText: 'Data de nascimento'
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Sexo'
+                ),
+                value: dropdownValueSex,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValueSex = newValue!;
+                  });
+                },
+                items: <String>['Masculino', 'Feminino']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               ),
-              onTap: () => onTapFunction(context: context),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Número de contato'
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Profissão'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Escolaridade'
+                ),
+                value: dropdownValueStudy,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValueStudy = newValue!;
+                  });
+                },
+                items: <String>['Fundamental incompleto', 'Fundamental completo', 'Médio incompleto', 'Médio completo', 'Superior incompleto', 'Superior completo']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Peso (Kg)'
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'Altura (cm)'
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border:OutlineInputBorder(),
+                  labelText: 'IMC'
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Theme.of(context).colorScheme.tertiary
+                ),
+              ),
+              onPressed: (){}, 
+              child: const Text(
+                'Salvar',
+                style: TextStyle(
+                  color: Colors.white
+                )
+              )
             )
           ],
         ),
