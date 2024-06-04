a9ff396978e4add84bcd9f69c7e2dc8aa308aafd
Isadora Ramos <isadorachb@gmail.com>
2024-06-04 11:57:31 -0300

    Create question list

--- lib/pages/questionnaire_page.dart
+++ lib/pages/questionnaire_page.dart
@@ -1,9 +1,40 @@
+import 'package:clini_dor/models/question.dart';
+import 'package:clini_dor/pages/question_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
-class QuestionnairePage extends StatelessWidget {
+class QuestionnairePage extends StatefulWidget {
 
   const QuestionnairePage({super.key});
+
+  @override
+  State<QuestionnairePage> createState() => _QuestionnairePageState();
+}
+
+class _QuestionnairePageState extends State<QuestionnairePage> {
+
+  final List<Question> _questions = [
+    Question(questionText: "Pergunta 1", answers: ["Sim", "Não"], isMultipleChoice: false),
+    Question(questionText: "Pergunta 2", answers: ["Sim", "Não"], isMultipleChoice: false)
+  ];
+
+  int _selectedIndex = 1;
+
+  void previousQuestion(){
+    setState(() {
+      if(_selectedIndex > 0){
+        _selectedIndex--;
+      }
+    });
+  }
+
+  void nextQuestion(){
+    setState(() {
+      if(_selectedIndex < _questions.length - 1){
+        _selectedIndex++;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +52,28 @@ class QuestionnairePage extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: const Center(child: Text('Questionário'))
+      body: QuestionPage(question: _questions[_selectedIndex]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FloatingActionButton(
+              heroTag: "btn1",
+              backgroundColor: Theme.of(context).colorScheme.tertiary,
+              onPressed: previousQuestion,
+              child: Icon(Icons.navigate_before),
+            ),
+            FloatingActionButton(
+              heroTag: "btn2",
+              backgroundColor: Theme.of(context).colorScheme.tertiary,
+              onPressed: nextQuestion,
+              child: Icon(Icons.navigate_next),
+            )
+          ],
+        ),
+      )
     );
   }
 }
